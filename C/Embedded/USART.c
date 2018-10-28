558f46d871d28daf569b5ab0d66c7cbd120d2c16
Gerard van Dijk <gerardvandijk2@gmail.com>
2018-10-28 02:21:15 +0200

    Update naar RS-232 Protocol 1.1

--- C/Embedded/USART.c
+++ C/Embedded/USART.c
@@ -2,8 +2,10 @@
 #include "USART.h"
 #include "HC_SR04.h"
 
-uint8_t incoming;
-uint16_t outgoing;
+uint8_t incoming = 0;
+uint8_t instruction = 0;
+uint16_t value = 0;
+uint16_t outgoing = 0;
 
 /*
  * Initialiseer de USART communicatie
@@ -24,56 +26,121 @@ void USART_Init(void) {
 }
 
 /*
+ * Voer de schrijfinstructies uit
+ */
+void handleInstruction(void) {
+	switch(instruction) {
+		case 0xe1:
+			// TODO: Pas de minimale afstand aan
+			break;
+		
+		case 0xe2:
+			// TODO: Pas de maximale afstand aan
+			break;
+		
+		case 0xf1:
+			// TODO: Pas de lichtintensiteit threshold aan
+			break;
+		
+		case 0xf2:
+			// TODO: Pas de temperatuur threshold aan
+			break;
+	}
+	instruction = 0;
+	value = 0;
+	outgoing = 0xff;
+	SCH_Add_Task(USART_Transmit_Low, 0, 0);
+}
+
+/*
  * Lees instructies
  * 0x81		Lees licht sensor
  * 0x82		Lees temperatuur sensor
- * 0x83		Lees afstand
- * 0x84		Lees constante MIN_DIS
- * 0x85		Lees constante MAX_DIS
+ * 0x91		Lees afstand
+ * 0x92		Lees constante MIN_DIS
+ * 0x93		Lees constante MAX_DIS
  * 
  * Actie instrucites
- * 0x8b		Rol zonnescherm uit
- * 0x8c		Rol zonnescherm in
- * 0x8d		Stop in- of uitrollen
+ * 0xc1		Rol zonnescherm uit
+ * 0xc2		Rol zonnescherm in
+ * 0xc3		Stop in- of uitrollen
+ * 0xc4		Toggle automatische bediening licht
+ * 0xc5		Toggle automatische bediening temeratuur
+ *
+ * Schrijf instructies
+ * 0xe1		Pas de minimale afstand aan
+ * 0xe2		Pas de maximale afstand aan
+ * 0xf1		Pas de lichtintensiteit threshold aan
+ * 0xf2		Pas de temperatuur threshold aan
  */ 
-void handleCommand() {
-	switch(incoming) {
-		case 0x81:
-			outgoing = 0xee;
-			break;
+void handleCommand(void) {
+	if (instruction != 0) {
+		if (value != 0) {
+			value += (incoming << 8);
+			handleInstruction();
+		} else {
+			value = incoming;
+			if ((instruction >> 4) == 0xe) {
+				handleInstruction();
+			} else if ((instruction >> 4) == 0xf) {
+				outgoing = 0xf0;
+				SCH_Add_Task(USART_Transmit_Low, 0, 0);
+			}
+		}
+	} else {
+		if ((incoming >> 4) == 0xe || (incoming >> 4) == 0xf) {
+			instruction = incoming;
+			outgoing = 0xf0;
+			SCH_Add_Task(USART_Transmit_Low, 0, 0);
+		} else {
+			switch(incoming) {
+				case 0x81:
+				outgoing = 0;
+				break;
 			
-		case 0x82:
-			outgoing = 16;
-			break;
+				case 0x82:
+				outgoing = 0;
+				break;
 			
-		case 0x83:
-			outgoing = Ultrasoon_Trigger();
-			break;
+				case 0x91:
+				outgoing = Ultrasoon_Trigger();
+				break;
 			
-		case 0x84:
-			outgoing = getMinimumDistance();
-			break;
+				case 0x92:
+				outgoing = getMinimumDistance();
+				break;
 			
-		case 0x85:
-			outgoing = getMaximumDistance();
-			break;
+				case 0x93:
+				outgoing = getMaximumDistance();
+				break;
+			
+				case 0xc1:
+				break;
+			
+				case 0xc2:
+				break;
+			
+				case 0xc3:
+				break;
 				
-		case 0x8b:
-			break;
+				case 0xc4:
+				break;
+				
+				case 0xc5:
+				break;
 			
-		case 0x8c:
-			break;
-			
-		case 0x8d:
-			break;
-			
-		default:
-			outgoing = 0xff;
-			break;
+				default:
+				outgoing = 0xff;
+				break;
+			}
+			if ((incoming >> 4) == 0x81 || (incoming >> 4) == 0x82) {
+				SCH_Add_Task(USART_Transmit_High, 0, 0);
+				SCH_Add_Task(USART_Transmit_Low, 1, 0);
+			} else {
+				SCH_Add_Task(USART_Transmit_Low, 0, 0);
+			}
+		}
 	}
-	
-	SCH_Add_Task(USART_Transmit_High, 0, 0);
-	SCH_Add_Task(USART_Transmit_Low, 1, 0);
 }
 
 /*
--- C/Embedded/USART.h
+++ C/Embedded/USART.h
@@ -2,8 +2,8 @@
 void USART_Init(void);
 void USART_Transmit_High(void);
 void USART_Transmit_Low(void);
+void USART_Transmit_Ready(void);
 void USART_Receive(void);
-void Toggle_LED(void);
 void waitForReceive();
 
 #define UBRRVAL 51
