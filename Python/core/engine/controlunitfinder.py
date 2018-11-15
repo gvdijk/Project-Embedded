0a3435c9a50fa107a59c40e153070f7ec0b77851
Gerard van Dijk <gerardvandijk2@gmail.com>
2018-11-15 12:41:03 +0100

    Comments, indentation

--- Python/core/engine/controlunitfinder.py
+++ Python/core/engine/controlunitfinder.py
@@ -9,33 +9,36 @@ from Python.event.event import Event
 
 
 class ControlUnitFinder:
-    class ControlUnitFoundEvent(Event):
-        def __init__(self, global_identifier: str):
-            super().__init__(global_identifier)
+	class ControlUnitFoundEvent(Event):
+		def __init__(self, global_identifier: str):
+			super().__init__(global_identifier)
 
-    def __init__(self):
-        self.control_units = {}
-        self.control_unit_found_event = ControlUnitFinder.ControlUnitFoundEvent('control_unit_found')
+	def __init__(self):
+		self.control_units = {}
+		self.control_unit_found_event = ControlUnitFinder.ControlUnitFoundEvent('control_unit_found')
 
-    def poll(self):
-        ports_found = serial.tools.list_ports.comports()
+	def poll(self):
+		# Get a list of all open COM ports, and iterate through them
+		ports_found = serial.tools.list_ports.comports()
+		for port in ports_found:
 
-        for port in ports_found:
+			def check_type(deviceport):
+				try:
+					# Attempt to lay a connection, wait for it to establish, and read the sensor type
+					control_unit = ControlUnit(deviceport.device)
+					time.sleep(2)
+					control_unit.type = control_unit.get_sensor_type()
 
-            def check_type(deviceport):
-                try:
-                    control_unit = ControlUnit(deviceport.device)
-                    time.sleep(2)
-                    control_unit.type = control_unit.get_sensor_type()
+					# Add the device if we got a useful result
+					if control_unit.type is not ControlUnit.Type.UNIDENTIFIED:
+						self.control_units[deviceport.device] = control_unit
+						self.control_unit_found_event.call(control_unit=control_unit)
+				except:
+					print("already connected {} as {}".format(port.device, self.control_units.keys()))
 
-                    if control_unit.type is not ControlUnit.Type.UNIDENTIFIED:
-                        self.control_units[deviceport.device] = control_unit
-                        self.control_unit_found_event.call(control_unit=control_unit)
-                except:
-                    print("already connected {} and {}".format(port.device, self.control_units.keys()))
+			# If the port is not already in use by us, attempt to open it
+			if port.device not in self.control_units.keys():
+				thread = Thread(target=check_type(port)).start()
 
-            if port.device not in self.control_units.keys():
-                thread = Thread(target=check_type(port)).start()
-
-    def remove_control_unit(self, control_unit):
-        self.control_units.pop(control_unit.port)
+	def remove_control_unit(self, control_unit):
+		self.control_units.pop(control_unit.port)
